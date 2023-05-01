51f322f53d75fa16a46d30654c70e4e8375dcc89
JT <60189885+Bethec0de@users.noreply.github.com>
2023-05-01 16:21:18 +0100

    Connecting to local DB

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import com.twt.business.*;
+import java.util.UUID;
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        BodyParts bp = new BodyParts();
+        System.out.println(bp.parts);
+    }
+
+
+}
